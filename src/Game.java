5341c8755fc042cde08fb849bf59637ef2181d10
AceMouse <humle000@gmail.com>
2022-05-02 10:03:35 +0200

    make interface better and board less cluttered

--- src/Game.java
+++ src/Game.java
@@ -16,29 +16,38 @@ public class Game {
                 new OnePair(), new TwoPair(), new ThreePair(), new ThreeOfAKind(), new FourOfAKind(), new FiveOfAKind(), new TwoXThreeOfAKind(),
                 new SmallStraight(), new LargeStraight(), new Royal(), new FullHouse(), new Chance(), new Yatzy() };
         int[][] board = new int[rules.length][players];
+        boolean[][] used = new boolean[board.length][board[0].length];
         byte[] dice = new byte[6];
         int player = 0;
+
         while (true) {
             boolean[] keep = new boolean[6];
-            for (int i = 0; i < 3; i++) {
-                roll(dice, keep);
-                showBoard(rules, board, dice);
+            roll(dice, keep);
+            for (int i = 0; i < 2; i++) {
+                showBoard(rules, board,used, dice, player);
                 System.out.println("Which do you want to keep?");
                 var nextLine = sc.nextLine();
                 if ("d".equals(nextLine))
                     break;
                 for (var die : nextLine.split("")){
-                    keep[Integer.parseUnsignedInt(die)-1] = true;
+                    if ('1'<= die.charAt(0) && die.charAt(0)<='6')
+                        keep[Integer.parseUnsignedInt(die)-1] = true;
                 }
+                roll(dice, keep);
             }
-            System.out.println("Which rule do you want to use?");
-            var nextLine = sc.nextLine();
-            if ("q".equals(nextLine))
-                break;
-            for (int i = 0; i < rules.length; i++) {
-                if (rules[i].getName().equalsIgnoreCase(nextLine)){
-                    board[i][player] = rules[i].getScore(dice);
+            showBoard(rules, board,used, dice, player);
+            outer:
+            while (true) {
+                System.out.println("Which rule do you want to use?");
+                var nextLine = sc.nextLine();
+                if ("q".equals(nextLine))
                     break;
+                for (int i = 0; i < rules.length; i++) {
+                    if (rules[i].getName().equalsIgnoreCase(nextLine) && !used[i][player]) {
+                        board[i][player] = rules[i].getScore(dice);
+                        used[i][player] = true;
+                        break outer;
+                    }
                 }
             }
             player = (player +1)%players;
@@ -50,20 +59,20 @@ public class Game {
                 dice[i] = (byte) rand.nextInt(1, 7);
         }
     }
-    private static void showBoard(Rule[] rules, int[][] board, byte[] dice){
+    private static void showBoard(Rule[] rules, int[][] board, boolean[][] used, byte[] dice, int player){
         System.out.printf("|%s|%s|",Util.rightPad("RULE", 20), Util.rightPad("POSSIBLE POINTS", 20));
         for (int i = 0; i < board[0].length; i++) {
             System.out.printf("%s|", Util.rightPad(String.valueOf(i+1), 4));
         }
         System.out.println();
         for (int i = 0; i < rules.length; i++) {
-            System.out.printf("|%s|%s|",Util.rightPad(rules[i].getName(), 20), Util.rightPad(String.valueOf(rules[i].getScore(dice)), 20));
+            System.out.printf("|%s|%s|",Util.rightPad(rules[i].getName(), 20), Util.rightPad(used[i][player]?"":String.valueOf(rules[i].getScore(dice)), 20));
             for (int j = 0; j < board[0].length; j++) {
-                System.out.printf("%s|", Util.rightPad(String.valueOf(board[i][j]), 4));
+                System.out.printf("%s|", Util.rightPad(board[i][j] == 0?(used[i][j]?"/":""):String.valueOf(board[i][j]), 4));
             }
             System.out.println();
         }
-        System.out.printf("|%s|%s|",Util.rightPad("Total", 20), Util.rightPad(" ", 20));
+        System.out.printf("|%s|%s|",Util.rightPad("Total", 20), Util.rightPad("", 20));
         for (int i = 0; i < board[0].length; i++) {
             int total = 0;
             for (int j = 0; j < board.length; j++) {
